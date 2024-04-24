6e9b160358b7f0cf50aee7d03446cd6567705710
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-04-24 10:34:15 +0630

    Initial commit

---
+++ src/main/java/org/jdc/authenticationfilters/AuthenticationFiltersApplication.java
@@ -0,0 +1,13 @@
+package org.jdc.authenticationfilters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationFiltersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationFiltersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jdc/authenticationfilters/AuthenticationFiltersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jdc.authenticationfilters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationFiltersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
